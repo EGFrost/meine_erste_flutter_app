2e99ea2efaca7e694bf97a259119bfc50456f6d2
EGFrost <maximilian.riedl@informatik.hs-fulda.de>
2019-10-22 17:09:12 +0200

    Immer das Unspezifische

--- lib/main.dart
+++ lib/main.dart
@@ -30,16 +30,19 @@ class _ErsteSeiteState extends State<ErsteSeite> {
       appBar: AppBar(
         title: Text("Tolle App!"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Image.network("https://images.unsplash.com/photo-1536315238512-4c8cebdaaf93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"),
-            buildRow("Thema:", "Mein Urlaub"),
-            buildRow("Reiseziel:", "South Korea"),
-            buildRow("Reisedauer:", "3 Wochen"),
-            buildRow("Aufenthaltsort:", "Seoul"),
-            buildRow("Wetter:", "Super"),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.network("https://images.unsplash.com/photo-1536315238512-4c8cebdaaf93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"),
+              buildRow("Thema:", "Mein Urlaub"),
+              buildRow("Reiseziel:", "South Korea"),
+              buildRow("Reisedauer:", "3 Wochen"),
+              buildRow("Aufenthaltsort:", "Seoul"),
+              buildRow("Wetter:", "Super"),
+            ],
+          ),
         ),
       ),
     );
